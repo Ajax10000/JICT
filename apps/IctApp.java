b596407eb171e2ad901804d2d7b14fbb3e8c08ab
Ajax10000 <david.de.leon.1000@att.net>
2023-01-18 18:13:52 -0600

    Added field of type MainFrame, which is set in method initInstance.
    -   The constructor now takes a parameter, an array of Strings representing command line options (for future implementation).
    -   In method initInstance, I added parameter args of type String[]. In that method I also deleted calls to Enable3dControls and LoadStdProfileSettings. Added a parameter to the MainFrame constructor. Replaced the call to pMainFrame.ShowWindow with mainFrame.setVisible. Replaced the call to pMainFrame.UpdateWindow with mainFrame.repaint(). Replaced the remove statement with a File.delete statement.
    -   Added main function, which instantiates an instance of IctApp. It passes the command line options to the constructor.
    -   Moved methods onAppAbout, onFileOpenImage, onFileOpenIctLog, onFileOpen to MainFrame.java.

--- apps/IctApp.java
+++ apps/IctApp.java
@@ -10,9 +10,13 @@ import frames.MainFrame;
 import globals.Globals;
 import globals.Preference;
 
+import java.io.File;
+
 import javax.swing.JFileChooser;
+import javax.swing.SwingUtilities;
 
 public class IctApp {
+    private MainFrame mainFrame;
     public CMultiDocTemplate m_pDocTemplateImage;
     public CMultiDocTemplate m_pDocTemplateText;
 
@@ -48,14 +52,15 @@ CIctApp theApp;
  */
 
     // This constructor came from ICT20.CPP
-    public IctApp() {
+    public IctApp(String[] args) {
         // TODO: add construction code here,
         // Place all significant initialization in InitInstance
+        initInstance(args);
     } // IctApp ctor
 
 
     // This method came from ICT20.CPP
-    boolean initInstance() {
+    boolean initInstance(String args[]) {
         String msgText;
 
         // Standard initialization
@@ -63,10 +68,7 @@ CIctApp theApp;
         // of your final executable, you should remove from the following
         // the specific initialization routines you do not need.
 
-        Enable3dControls();
-
-        LoadStdProfileSettings();  // Load standard INI file options (including MRU)
-
+        /*
         // Register the application's document templates.  Document templates
         // serve as the connection between documents, frame windows and views.
 
@@ -84,27 +86,32 @@ CIctApp theApp;
 
         AddDocTemplate(m_pDocTemplateText);
         AddDocTemplate(m_pDocTemplateImage);
+        */
 
         // create main MDI Frame window
-        MainFrame pMainFrame = new MainFrame();
-        if (!pMainFrame.LoadFrame(IDR_MAINFRAME)) {
-            return false;
-        }
-        m_pMainWnd = pMainFrame;
+        this.mainFrame = new MainFrame(this);
 
-        if (m_lpCmdLine[0] != '\0') {
+        if ((args != null) && (args.length > 0)) {
             // TODO: add command line processing here
         }
 
-        // The main window has been initialized, so show and update it.
-        m_nCmdShow = SW_SHOWMAXIMIZED;            
-        pMainFrame.ShowWindow(m_nCmdShow);
-        pMainFrame.UpdateWindow();
+        // The main window has been initialized, so show and update it.       
+        mainFrame.setVisible(true);
+        mainFrame.repaint();
 
         Globals.ictPreference = new Preference();
 
         // Remove the old ict log and create a new one
-        remove(Globals.ictPreference.getPath(Preference.ProcessLog));
+        String oldLogFilePath = Globals.ictPreference.getPath(Preference.ProcessLog);
+        File oldLogFile = new File(oldLogFilePath);
+        if (oldLogFile.exists()) {
+            boolean deleted = oldLogFile.delete();
+            if(!deleted) {
+                // TODO: Write a message on mainFrame
+            }
+        }
+
+
         time_t theTime;
         time(theTime);
         msgText = "ICT Process Log.  " + ctime(theTime);
@@ -114,62 +121,11 @@ CIctApp theApp;
     } // initInstance
 
 
-    // Called when the user clicks on the Help menu
-    // MENUITEM "&About ICT 2.0...",           ID_APP_ABOUT
-    // ON_COMMAND(ID_APP_ABOUT, OnAppAbout)
-    public void onAppAbout() {
-        AboutDlg aboutDlg = new AboutDlg(null, true);
-        aboutDlg.setVisible(true);
-    } // onAppAbout
-
-    /////////////////////////////////////////////////////////////////////////////
-    // CIctApp commands
-
-    // Called when the user selects the Open Image menu item from the File menu
-    // MENUITEM "Open Image",                  ID_FILE_OPENIMAGE
-    // ON_COMMAND(ID_FILE_OPENIMAGE, OnFileOpenimage)
-    public void onFileOpenImage() {
-        // change to the default output file directory (indicated in the ICT preference object)
-        _chdir(Globals.ictPreference.getPath(Preference.OutputImageDirectory)); 
-
-        // TODO: Replace with JFileChooser
-        // CFileDialog dlg = new CFileDialog(true, "bmp", "*.bmp");
-        JFileChooser dlg = new JFileChooser();
-        dlg.setFileSelectionMode(JFileChooser.FILES_ONLY);
-        int showDlgResult = dlg.showDialog(null, "Select image name");
-
-        if (showDlgResult == JFileChooser.APPROVE_OPTION) {
-            m_pDocTemplateImage.OpenDocumentFile(dlg.getSelectedFile().getName());
-        }
-    } // onFileOpenImage
-
-
-    // Called when the user selects the Open ICT Log menu item from the File menu
-    // MENUITEM "Open ICT Log",                ID_FILE_OPENICTLOG
-    // ON_COMMAND(ID_FILE_OPENICTLOG, OnFileOpenictlog)
-    public void onFileOpenIctLog() {
-        String processLogPath;
-
-        processLogPath = Globals.ictPreference.getPath(Preference.ProcessLog);
-        m_pDocTemplateText.OpenDocumentFile(processLogPath);
-    } // onFileOpenIctLog
-
-
-    // Called when the user selects the Open Scene menu item from the File menu
-    // MENUITEM "&Open Scene...\tCtrl+O",      ID_FILE_OPEN
-    // ON_COMMAND(ID_FILE_OPEN, OnFileOpen)
-    public void onFileOpen() {
-        // Change to the default scene file directory (indicated in the ICT preference object)
-        _chdir(Globals.ictPreference.getPath(Preference.SceneFileDirectory));
-
-        // TODO: Replace with JFileChooser
-        // CFileDialog dlg = new CFileDialog(true, "scn", "*.scn");
-        JFileChooser dlg = new JFileChooser();
-        dlg.setFileSelectionMode(JFileChooser.FILES_ONLY);
-        int showDlgResult = dlg.showDialog(null, "Select scene file");
-
-        if (showDlgResult == JFileChooser.APPROVE_OPTION) {
-            m_pDocTemplateText.OpenDocumentFile(dlg.getSelectedFile().getName());
-        }
-    } // onFileOpen
+    public void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new IctApp(args);
+            }
+        });
+    } // main
 } // class IctApp
