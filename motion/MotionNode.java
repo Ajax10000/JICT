345720b1dc49071a771c84ac68c8161db33fa66d
Ajax10000 <david.de.leon.1000@att.net>
2023-01-29 13:25:50 -0600

    Added a constructor and formatted code.

--- motion/MotionNode.java
+++ motion/MotionNode.java
@@ -4,10 +4,18 @@ import java.util.StringTokenizer;
 
 public class MotionNode {
    public int nodenum;
-   public float tx,ty,tz;
-   public float rx,ry,rz;
-   public float sx,sy,sz;
+   public float tx, ty, tz;
+   public float rx, ry, rz;
+   public float sx, sy, sz;
    public float alpha;
+
+
+   // Called from:
+   //     SceneList.getViewMatrix
+   public void MotionNode() {
+      clear();
+   }
+
 
    // This method came from MOTION.CPP
    public void copy(MotionNode mn) {
@@ -28,6 +36,7 @@ public class MotionNode {
       this.alpha = mn.alpha;
    } // copy
   
+
    // This method came from MOTION.CPP
    public void clear() {
       nodenum = 0;
@@ -47,6 +56,7 @@ public class MotionNode {
       alpha = 0.0f;
    } // clear
 
+
    // This method came from MOTION.CPP
    public int read(String psBuffer) {
       StringTokenizer strTok;
@@ -58,70 +68,81 @@ public class MotionNode {
    
       strTok = new StringTokenizer(psBuffer, " ");
       aNodeNum = strTok.nextToken(" ");
-      if(aNodeNum.length() > 0)
+      if(aNodeNum.length() > 0) {
          nodenum = Integer.parseInt(aNodeNum);
-      else
+      } else {
          return 1;
+      }
    
       anrx = strTok.nextToken(" ");
-      if(anrx.length() > 0)
+      if(anrx.length() > 0) {
          rx = Float.parseFloat(anrx);
-      else
+      } else {
          return 2;
+      }
    
       anry = strTok.nextToken(" ");
-      if(anry.length() > 0)
+      if(anry.length() > 0) {
          ry = Float.parseFloat(anry);
-      else
+      } else {
          return 3;
+      }
    
       anrz = strTok.nextToken(" ");
-      if(anrz.length() > 0)
+      if(anrz.length() > 0) {
          rz = Float.parseFloat(anrz);
-      else
+      } else {
          return 4;
+      }
    
       ansx = strTok.nextToken(" ");
-      if(ansx.length() > 0)
+      if(ansx.length() > 0) {
          sx = Float.parseFloat(ansx);
-      else
+      } else {
          return 5;
+      }
    
       ansy = strTok.nextToken(" ");
-      if(ansy.length() > 0)
+      if(ansy.length() > 0) {
          sy = Float.parseFloat(ansy);
-      else
+      } else {
          return 6;
+      }
    
       ansz = strTok.nextToken(" ");
-      if(ansz.length() > 0)
+      if(ansz.length() > 0) {
          sz = Float.parseFloat(ansz);
-      else
+      } else {
          return 7;
+      }
    
       antx = strTok.nextToken(" ");
-      if(antx.length() > 0)
+      if(antx.length() > 0) {
          tx = Float.parseFloat(antx);
-      else
+      } else {
          return 8;
+      }
    
       anty = strTok.nextToken(" ");
-      if(anty.length() > 0)
+      if(anty.length() > 0) {
          ty = Float.parseFloat(anty);
-      else
+      } else {
          return 9;
+      }
    
       antz = strTok.nextToken(" ");
-      if(antz.length() > 0)
+      if(antz.length() > 0) {
          tz = Float.parseFloat(antz);
-      else
+      } else {
          return 10;
+      }
    
       anAlpha = strTok.nextToken(" ");
-      if(anAlpha.length() > 0)
+      if(anAlpha.length() > 0) {
          alpha = Float.parseFloat(anAlpha);
-      else
+      } else {
          return 11;
+      }
    
       return 0;
    } // read
