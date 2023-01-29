33f4621ebb0c82cc209976a3e5f968d8bdc3b0e2
Ajax10000 <david.de.leon.1000@att.net>
2023-01-29 14:23:31 -0600

    Added many new fields, most of them being Java Swing components. Modified the constructor. It now sets field mMainFrame. It also calls setDefaultCloseOperation, so the appropriate action is taken when the user closes the dialog. Added methods addRotationFields and addScaleFields, which are called from the constructor. -Modified method onOK. Now before closing the code saves the value of the fields that the user populated in the dialog into some fields of MainFrame.java.

--- dialogs/WarpParamDlg.java
+++ dialogs/WarpParamDlg.java
@@ -1,27 +1,58 @@
 package dialogs;
 
+import frames.MainFrame;
+
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 // This dialog is displayed when the user selects the 
 // "Warp Image..." menu item from the Tools menu.
 // See method onToolsWarpImage of the MainFrame class.
 // To see what it should look like, see Figure D.7 on p 280 of the book.
-public class WarpParamDlg extends JDialog {
+public class WarpParamDlg extends JDialog implements ActionListener {
+    private MainFrame mMainFrame;
+
+    // EDITTEXT        IDC_EDITx,59,20,35,14,ES_AUTOHSCROLL
     // DDX_Text(pDX, IDC_EDITx, m_rx);
 	public String	m_rx;
 
-    // DDX_Text(pDX, IDC_EDITxScale, m_sx);
-	public String	m_sx;
-
+    // EDITTEXT        IDC_EDITy,59,37,35,14,ES_AUTOHSCROLL
     // DDX_Text(pDX, IDC_EDITy, m_ry);
 	public String	m_ry;
 
+    // EDITTEXT        IDC_EDITz,59,53,35,14,ES_AUTOHSCROLL
+    // DDX_Text(pDX, IDC_EDITz, m_rz);
+	public String	m_rz;
+
+    // EDITTEXT        IDC_EDITxScale,60,95,35,14,ES_AUTOHSCROLL
+    // DDX_Text(pDX, IDC_EDITxScale, m_sx);
+	public String	m_sx;
+
+    // EDITTEXT        IDC_EDITyScale,60,111,35,14,ES_AUTOHSCROLL
     // DDX_Text(pDX, IDC_EDITyScale, m_sy);
 	public String	m_sy;
 
-    // DDX_Text(pDX, IDC_EDITz, m_rz);
-	public String	m_rz;
+    private JTextField fldRotAboutXAxis;
+    private JTextField fldRotAboutYAxis;
+    private JTextField fldRotAboutZAxis;
+
+    private JTextField fldSclAlongXAxis;
+    private JTextField fldSclAlongYAxis;
+
+    private JButton btnOK;
+    private JButton btnCancel;
+
+    private JPanel pnlRotAngleGroup;
+    private JPanel pnlSclFactorGroup;
 
 /*
 class CWarpParamDlg : public CDialog
@@ -65,16 +96,84 @@ protected:
     //     MainFrame.onToolsWarpImage
     public WarpParamDlg(JFrame pParent, boolean pModal) {
         super(pParent, pModal);
+        mMainFrame = (MainFrame)pParent;
 
-        //{{AFX_DATA_INIT(CWarpParamDlg)
+        setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+
         m_rx = "0.0";
         m_sx = "1.0";
         m_ry = "0.0";
         m_sy = "1.0";
         m_rz = "0.0";
-        //}}AFX_DATA_INIT
+
+        addRotationFields();
+        addScaleFields();
+        btnOK = new JButton("OK");
+        btnCancel = new JButton("Cancel");
+
+        add(pnlRotAngleGroup);
+        add(pnlSclFactorGroup);
+        add(btnOK);
+        add(btnCancel);
     } // WarpParamDlg ctor
 
+
+    // Called from:
+    //     constructor
+    private void addRotationFields() {
+        Dimension fldSize = new Dimension(50, 30);
+        JLabel lblRotX = new JLabel("X-Axis: ");
+        JLabel lblRotY = new JLabel("Y-Axis: ");
+        JLabel lblRotZ = new JLabel("Z-Axis: ");
+
+        fldRotAboutXAxis = new JTextField();
+        fldRotAboutXAxis.setSize(fldSize);
+        fldRotAboutXAxis.setPreferredSize(fldSize);
+
+        fldRotAboutYAxis = new JTextField();
+        fldRotAboutYAxis.setSize(fldSize);
+        fldRotAboutYAxis.setPreferredSize(fldSize);
+
+        fldRotAboutZAxis = new JTextField();
+        fldRotAboutZAxis.setSize(fldSize);
+        fldRotAboutZAxis.setPreferredSize(fldSize);
+
+        pnlRotAngleGroup = new JPanel();
+        pnlRotAngleGroup.setBorder(BorderFactory.createTitledBorder("Rotation Angles in Degrees"));
+
+        pnlRotAngleGroup.add(lblRotX);
+        pnlRotAngleGroup.add(fldRotAboutXAxis);
+
+        pnlRotAngleGroup.add(lblRotY);
+        pnlRotAngleGroup.add(fldRotAboutYAxis);
+
+        pnlRotAngleGroup.add(lblRotZ);
+        pnlRotAngleGroup.add(fldRotAboutZAxis);
+    }
+
+
+    // Called from:
+    //     constructor
+    private void addScaleFields() {
+        JLabel lblSclX = new JLabel("X-Axis: ");
+        JLabel lblSclY = new JLabel("Y-Axis: ");
+
+        fldSclAlongXAxis = new JTextField();
+        fldSclAlongYAxis = new JTextField();
+
+        pnlSclFactorGroup = new JPanel();
+        pnlSclFactorGroup.setBorder(BorderFactory.createTitledBorder("Scale Factors"));
+
+        pnlSclFactorGroup.add(lblSclX);
+        pnlSclFactorGroup.add(fldSclAlongXAxis);
+
+        pnlSclFactorGroup.add(lblSclY);
+        pnlSclFactorGroup.add(fldSclAlongYAxis);
+    }
+
+    public void actionPerformed(ActionEvent ae) {
+
+    }
 
     /*
     // This method came from WARPPARMDLG.CPP
@@ -94,6 +193,13 @@ protected:
 
     // This method came from WARPPARMDLG.CPP
     void onOK() {
-        // TODO: Add extra validation here
+        // Save the values the user entered in the WarpParamDlg dialog
+        mMainFrame.mWarpRotateX = Float.valueOf(m_rx);
+        mMainFrame.mWarpRotateY = Float.valueOf(m_ry);
+        mMainFrame.mWarpRotateZ = Float.valueOf(m_rz);
+        mMainFrame.mWarpScaleX  = Float.parseFloat(m_sx);
+        mMainFrame.mWarpScaleY  = Float.parseFloat(m_sy);
+
+        mMainFrame.onWarpParamDlgClosed();
     } // onOk
 } // class WarpParamDlg
