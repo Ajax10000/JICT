75d2fa634ba07b5329d012589c503fe48542618c
Ajax10000 <david.de.leon.1000@att.net>
2023-02-03 16:33:02 -0600

    Added comments. Modified methods getFirstLastFrame, getNode, and readMotion due to changes in the names of the fields in class MotionNode.

--- motion/MotionPath.java
+++ motion/MotionPath.java
@@ -2,6 +2,9 @@ package motion;
 
 import globals.Globals;
 
+// A MotionPath object consists of a list of MotionNode objects stored in field mNodes.
+// The field miNumNodes contains the number of MotionNode (aka keyframes) in the MotionPath.
+// See pages 123, 127, and 131 of the book Visual Special Effects Toolkit in C++.
 public class MotionPath {
 
   private boolean mbAllocated;
@@ -9,29 +12,34 @@ public class MotionPath {
 
   public int miNumNodes;
   public MotionNode[] mNodes;
-  // public int read(char *);
-  // public int readMotion(char *);
-  // public int write(char *);
-  // public int getNode(int, motionNode *);
-  // public motionPath(void);
-  // public ~motionPath(void);
-  // public void getFirstLastFrame(int *firstFrame, int *lastFrame);
+  
+  // public int read(char *); ---- NOT IMPLEMENTED, couldn't find in C++ code ----
+  // public int readMotion(char *); - implemented
+  // public int write(char *); ---- NOT IMPLEMENTED, couldn't find in C++ code ----
+  // public int getNode(int, motionNode *); - implemented
+  // public motionPath(void); - implemented
+  // public ~motionPath(void); - implemented (as method finalize)
+  // public void getFirstLastFrame(int *firstFrame, int *lastFrame); - implemented
 
-    // This constructor came from MOTION.CPP
+    // This constructor originally came from MOTION.CPP
+    // Called from:
+    //     SceneElement ctor
     public MotionPath() {
         mbAllocated = false;
         mNodes = null;
         miNumNodes = 0;
     } // MotionPath ctor
 
-    // This destructor came from MOTION.CPP
+    // This destructor originally came from MOTION.CPP
     public void finalize() {
       /*
         if(allocated) delete nodes;
       */
     } // finalize
 
-    // This method came from MOTION.CPP
+    // This method originally came from MOTION.CPP
+    // Called from:
+    //     readMotion
     private void allocate(int piNum) {
         if(mbAllocated) {
             miNumNodes = 0;
@@ -48,18 +56,30 @@ public class MotionPath {
     } // allocate
   
     
-    // This method came from MOTION.CPP
+    // This method originally came from MOTION.CPP
+    // 
+    // Returns the first (pIFirstFrame) and last (pILastFrame) frame 
+    // numbers identified in the motion file. See p 131 of 
+    // Visual Special Effects Toolkit in C++
+    //
     // Called from:
+    //     getNode
     //     SceneList.preview
     //     SceneList.previewStill
     //     SceneList.render
     public void getFirstLastFrame(Integer pIFirstFrame, Integer pILastFrame) {
-        pIFirstFrame = mNodes[0].nodenum;
-        pILastFrame = mNodes[miNumNodes - 1].nodenum;
+        pIFirstFrame = mNodes[0].miNodeNum;
+        pILastFrame = mNodes[miNumNodes - 1].miNodeNum;
     } // getFirstLastFrame
 
 
-    // This method came from MOTION.CPP
+    // This method originally came from MOTION.CPP
+    //
+    // Returns an interpolated MotionNode object pMtnNode, 
+    // given any frame number piFrameNumber that lies 
+    // between the first and last frame numbers. See p 131 of
+    // Visual Special Effects Toolkit in C++
+    //
     // Called from
     //     SceneList.getViewMatrix
     //     SceneList.preview
@@ -82,7 +102,7 @@ public class MotionPath {
         }
       
         for(i = 0; i < miNumNodes; i++) {
-            if(mNodes[i].nodenum == piFrameNumber) {
+            if(mNodes[i].miNodeNum == piFrameNumber) {
                 pMtnNode.copy(mNodes[i]);
                 return 0;
             }
@@ -90,8 +110,8 @@ public class MotionPath {
 
         i = 0;
         while(i < (miNumNodes - 1)) {
-            iPrevNode = mNodes[i].nodenum;
-            iNextNode = mNodes[i + 1].nodenum;
+            iPrevNode = mNodes[i].miNodeNum;
+            iNextNode = mNodes[i + 1].miNodeNum;
             if(iPrevNode < piFrameNumber && iNextNode > piFrameNumber) break;
             i++;
         } // while
@@ -104,46 +124,53 @@ public class MotionPath {
         fDiff = iNextNode - iPrevNode;
         fMult = (float)(piFrameNumber - iPrevNode) / fDiff;
       
-        // Process scale point
-        fDist = mNodes[i + 1].sx - mNodes[i].sx;
-        pMtnNode.sx = mNodes[i].sx + fDist * fMult;
+        // Set the interpolated MotionNode's scale values mfSx, mfSy, and mfSz
+        fDist = mNodes[i + 1].mfSx - mNodes[i].mfSx;
+        pMtnNode.mfSx = mNodes[i].mfSx + fDist * fMult;
       
-        fDist = mNodes[i + 1].sy - mNodes[i].sy;
-        pMtnNode.sy = mNodes[i].sy + fDist * fMult;
+        fDist = mNodes[i + 1].mfSy - mNodes[i].mfSy;
+        pMtnNode.mfSy = mNodes[i].mfSy + fDist * fMult;
       
-        fDist = mNodes[i + 1].sz - mNodes[i].sz;
-        pMtnNode.sz = mNodes[i].sz + fDist * fMult;
+        fDist = mNodes[i + 1].mfSz - mNodes[i].mfSz;
+        pMtnNode.mfSz = mNodes[i].mfSz + fDist * fMult;
       
-        // Process rotation point
-        fDist = mNodes[i + 1].rx - mNodes[i].rx;
-        pMtnNode.rx = mNodes[i].rx + fDist * fMult;
+        // Set the interpolated MotionNode's rotation values mfRx, mfRy and mfRz
+        fDist = mNodes[i + 1].mfRx - mNodes[i].mfRx;
+        pMtnNode.mfRx = mNodes[i].mfRx + fDist * fMult;
       
-        fDist = mNodes[i + 1].ry - mNodes[i].ry;
-        pMtnNode.ry = mNodes[i].ry + fDist * fMult;
+        fDist = mNodes[i + 1].mfRy - mNodes[i].mfRy;
+        pMtnNode.mfRy = mNodes[i].mfRy + fDist * fMult;
       
-        fDist = mNodes[i + 1].rz - mNodes[i].rz;
-        pMtnNode.rz = mNodes[i].rz + fDist * fMult;
+        fDist = mNodes[i + 1].mfRz - mNodes[i].mfRz;
+        pMtnNode.mfRz = mNodes[i].mfRz + fDist * fMult;
       
-        // Process translation point
-        fDist = mNodes[i + 1].tx - mNodes[i].tx;
-        pMtnNode.tx = mNodes[i].tx + fDist * fMult;
+        // Set the interpolated MotionNode's translation values mfTx, mfTy and mfTz
+        fDist = mNodes[i + 1].mfTx - mNodes[i].mfTx;
+        pMtnNode.mfTx = mNodes[i].mfTx + fDist * fMult;
       
-        fDist = mNodes[i + 1].ty - mNodes[i].ty;
-        pMtnNode.ty = mNodes[i].ty + fDist * fMult;
+        fDist = mNodes[i + 1].mfTy - mNodes[i].mfTy;
+        pMtnNode.mfTy = mNodes[i].mfTy + fDist * fMult;
       
-        fDist = mNodes[i + 1].tz - mNodes[i].tz;
-        pMtnNode.tz = mNodes[i].tz + fDist * fMult;
+        fDist = mNodes[i + 1].mfTz - mNodes[i].mfTz;
+        pMtnNode.mfTz = mNodes[i].mfTz + fDist * fMult;
       
-        // Process alpha
-        fDist = mNodes[i + 1].alpha - mNodes[i].alpha;
-        pMtnNode.alpha = mNodes[i].alpha + fDist * fMult;
-        pMtnNode.nodenum = piFrameNumber;
+        // Set the interpolated MotionNode's alpha value mfAlpha
+        fDist = mNodes[i + 1].mfAlpha - mNodes[i].mfAlpha;
+        pMtnNode.mfAlpha = mNodes[i].mfAlpha + fDist * fMult;
+
+        // Set the interpolated MotionNode's node number miNodeNum
+        pMtnNode.miNodeNum = piFrameNumber;
 
         return 0;
     } // getNode
   
     
-    // This method came from MOTION.CPP
+    // This method originally came from MOTION.CPP
+    //
+    // This method reads a motion path file. These files are text files
+    // with a '.pth' extension. See p 127 and 129 - 130 of the book
+    // Visual Special Effects Toolkit in C++.
+    //
     // Called from:
     //     SceneElement ctor
     public int readMotion(String psPathName) {
@@ -189,7 +216,7 @@ public class MotionPath {
         iNodeCounter = 0;
         sKeyWord = Globals.getNextMotionLine(sText, iLineCounter, filein2);
         while(!sKeyWord.equalsIgnoreCase("EOF")) {
-            iStatus = nodes[iNodeCounter].read(sKeyWord);
+            iStatus = mNodes[iNodeCounter].read(sKeyWord);
             iNodeCounter++;
             sKeyWord = Globals.getNextMotionLine(sText, iLineCounter, filein2);
         }
