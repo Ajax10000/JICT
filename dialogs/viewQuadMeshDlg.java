c0f776203e86ee4d5e322ab68ec1e837174aa69a
Ajax10000 <david.de.leon.1000@att.net>
2023-02-09 09:52:31 -0600

    Initial commit.

---
+++ dialogs/viewQuadMeshDlg.java
@@ -0,0 +1,19 @@
+package dialogs;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+public class viewQuadMeshDlg extends JFrame {
+    
+    public viewQuadMeshDlg() {
+        QuadMeshDlg quadMeshDlg = new QuadMeshDlg(this, true);
+    }
+    
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new viewQuadMeshDlg();
+            }
+        });
+    }
+}
