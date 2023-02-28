651b2eca6a07b3ca273f8b9335d9517e677c0618
Ajax10000 <david.de.leon.1000@att.net>
2023-02-28 13:50:20 -0600

    Removed declaration of local variable makeTextureDlg in the constructor to remove an unused variable warning.

--- dialogs/viewMakeTextureDlg.java
+++ dialogs/viewMakeTextureDlg.java
@@ -6,7 +6,7 @@ import javax.swing.SwingUtilities;
 public class viewMakeTextureDlg extends JFrame {
     
     public viewMakeTextureDlg() {
-        MakeTextureDlg makeTextureDlg = new MakeTextureDlg(this, true);
+        new MakeTextureDlg(this, true);
     }
     
     public static void main(String[] args) {
