9e74ea2c72dc7de8c8d9e5c97856aa60c7f414b9
Ajax10000 <david.de.leon.1000@att.net>
2023-03-17 09:28:30 -0500

    Initial checkin

---
+++ dtos/BoundingBox.java
@@ -0,0 +1,13 @@
+package dtos;
+
+public class BoundingBox {
+    public int ixBeg, ixEnd;
+    public int iyBeg, iyEnd;
+
+    public BoundingBox() {
+        this.ixBeg = 0;
+        this.ixEnd = 0;
+        this.iyBeg = 0;
+        this.iyEnd = 0;
+    }
+} // class BoundingBox
---
+++ dtos/ColorAsBytes.java
@@ -0,0 +1,11 @@
+package dtos;
+
+public class ColorAsBytes {
+    public byte bytRed, bytGreen, bytBlue;
+
+    public ColorAsBytes() {
+        this.bytRed = (byte)0;
+        this.bytGreen = (byte)0;
+        this.bytBlue = (byte)0;
+    }
+} // class ColorAsBytes
