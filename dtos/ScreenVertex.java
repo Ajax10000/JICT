62f2f30693c8dcb498973e4d49350b2cf2a7e139
Ajax10000 <david.de.leon.1000@att.net>
2023-03-18 17:29:04 -0500

    Initial check in.

---
+++ dtos/ScreenVertex.java
@@ -0,0 +1,12 @@
+package dtos;
+
+// This is just a DTO, data transfer object.
+// Used to pass output parameters to methods.
+public class ScreenVertex {
+    public int iSx, iSy;
+
+    public ScreenVertex() {
+        this.iSx = 0;
+        this.iSy = 0;
+    }
+} // class ScreenVertex
