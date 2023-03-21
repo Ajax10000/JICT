ee81ee466ac6bbc22eebdcf972b6d936b3cb4bf4
Ajax10000 <david.de.leon.1000@att.net>
2023-03-21 15:39:55 -0500

    Changed class name to start with a capital O.

--- dtos/OneInt.java
+++ dtos/OneInt.java
@@ -2,10 +2,10 @@ package dtos;
 
 // This is just a DTO, data transfer object.
 // Used to pass output parameters to methods.
-public class oneInt {
+public class OneInt {
     public int i;
 
-    public oneInt() {
+    public OneInt() {
         this.i = 0;
     }
-} // class oneInt
+} // class OneInt
