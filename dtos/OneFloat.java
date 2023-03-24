4adb1cc05717a39296480fff23e5e6d829ffef8a
Ajax10000 <david.de.leon.1000@att.net>
2023-03-24 08:53:00 -0500

    Initial commit.

---
+++ dtos/OneFloat.java
@@ -0,0 +1,11 @@
+package dtos;
+
+// This is just a DTO, data transfer object.
+// Used to pass output parameters to methods.
+public class OneFloat {
+    public float f;
+
+    public OneFloat() {
+        this.f = 0.0f;
+    }
+} // class OneFloat
