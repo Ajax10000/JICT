7ac92b0fbc36d72727148e55eb9bb53df8b795f4
Ajax10000 <david.de.leon.1000@att.net>
2023-03-31 08:57:44 -0500

    -Modified method beep. Standardized the parameter names, added a try/catch statement, and added Global.statusPrint calls. -Modified method tone. Standardized the parameter names.

--- globals/Sound.java
+++ globals/Sound.java
@@ -6,23 +6,29 @@ import javax.sound.sampled.*;
 // https://stackoverflow.com/questions/3780406/how-to-play-a-sound-alert-in-a-java-application/6700039#6700039
 public class Sound {
     public static float F_SAMPLE_RATE = 8000f;
-    public static void beep(int hz, int msecs) throws LineUnavailableException {
-        tone(hz, msecs, 1.0);
-    }
+    
+    public static void beep(int piHz, int piMSecs) {
+        try {
+            tone(piHz, piMSecs, 1.0);
+        } catch (LineUnavailableException lue) {
+            Globals.statusPrint("Sound.beep: LineUnavailableException occurred when attempting beep.");
+            Globals.statusPrint("Sound.beep: Exception ignored.");
+        }
+    } // beep
 
-    public static void tone(int hz, int msecs, double vol) throws LineUnavailableException {
+    public static void tone(int piHz, int piMSecs, double pdVol) throws LineUnavailableException {
         byte[] buf = new byte[1];
         AudioFormat audioFmt = new AudioFormat(F_SAMPLE_RATE,8,1,true,false);     
         SourceDataLine sdl = AudioSystem.getSourceDataLine(audioFmt);
         sdl.open(audioFmt);
         sdl.start();
-        for (int i=0; i < msecs*8; i++) {
-              double fAngle = i / (F_SAMPLE_RATE / hz) * 2.0 * Math.PI;
-              buf[0] = (byte)(Math.sin(fAngle) * 127.0 * vol);
+        for (int i=0; i < piMSecs*8; i++) {
+              double fAngle = i / (F_SAMPLE_RATE / piHz) * 2.0 * Math.PI;
+              buf[0] = (byte)(Math.sin(fAngle) * 127.0 * pdVol);
               sdl.write(buf,0,1);
         }
         sdl.drain();
         sdl.stop();
         sdl.close();
-    }
+    } // tone
 } // class Sound
